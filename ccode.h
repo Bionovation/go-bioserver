73899cba22e3471a38f71b8ce80a9eabc3205cf4
Cgy <passer200@163.com>
2019-11-19 23:01:14 +0800

    提交

--- ccode.h
+++ ccode.h
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 void test(){
-    printf("hello there!");
+    printf("hello there!\n");
 }
 
+int readfile(char* path, char* buf, int32_t len){
+    FILE* pf = fopen(path, "rb");
+    if(pf == NULL) return 0;
+    fseek(pf, 0, SEEK_END);
+    long lSize = ftell(pf);
+    if(lSize > len) return 0;
+    rewind(pf);
+    fread(path, sizeof(char), lSize, pf);
+    fclose(pf);
+    return lSize;
+}
--- main.go
+++ main.go
@@ -8,15 +8,26 @@ import "C"
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"unsafe"
 )
 
 func main()  {
 	C.test();
 	r := gin.Default()
 	r.GET("/ping",handlePing)
+	r.GET("/image",handleImage)
 	r.Run(":9999")
 }
 
 func handlePing(c *gin.Context)  {
 	c.String(http.StatusOK,"pong!")
+}
+
+func handleImage(c *gin.Context)  {
+	str1 := "D:/test.jpg"
+	cstr1 := C.CString(str1)
+	defer C.free(unsafe.Pointer(cstr1))
+	b := make([]byte,1024*1024) // 1MB
+	C.readfile(cstr1, (*C.char)(unsafe.Pointer(&b[0])), C.int32_t(len(b)))
+	c.String(http.StatusOK,"ing!")
 }
